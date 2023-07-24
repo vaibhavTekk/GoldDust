d678785b8cb0ce649c298783cb2d8f0352986f2b
Chrsito Joby Antony <christotantony2003@gmail.com>
2023-07-24 21:07:15 +1000

    Clean up

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="bg-zinc-900">
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -5,3 +5,4 @@ import "./styles.css";
 export * from "./components/ui/button";
 export * from "./components/ui/card";
 export * from "./lib/utils";
+export * from "./components/ui/separator"
