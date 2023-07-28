5b2080037a456a65cbdf023ca6e7c13120c86793
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2023-07-28 21:00:15 +0530

    working sidebar logic

---
+++ apps/web/src/app/market/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+// import { Button, Card } from "ui";
+
+export const metadata: Metadata = {
+  title: "GoldDust",
+};
+
+export default function Home() {
+  return (
+    <div className="flex">
+      <h1>Market</h1>
+    </div>
+  );
+}
--- apps/web/src/components/Sidebar.jsx
+++ apps/web/src/components/Sidebar.jsx
@@ -1,16 +1,32 @@
 'use client'
 
-import { Element3,Setting2, EmptyWallet, StatusUp, DocumentText1, ArrangeHorizontalSquare, Notification, Profile2User, Headphone, SmsEdit, LogoutCurve} from 'iconsax-react';
+import { Element3,Setting2, EmptyWallet, StatusUp, DocumentText1, ArrangeHorizontalSquare, Notification, Profile2User, Headphone, SmsEdit, LogoutCurve, Logout} from 'iconsax-react';
 import Link from 'next/link';
+import { usePathname } from "next/navigation";
 
 export default function Sidebar(){
-    const links = [
+    const pathname = usePathname();
+
+    const sidebar = [
+    {title: 'Overview', links: [
         {label: 'Dashboard', path: '/', targetSegment: null, icon:<Element3 variant="Bold"/>},
         {label: 'Market', path: '/market', targetSegment: null, icon:<StatusUp />},
         {label: 'Portfolio', path: '/portfolio', targetSegment: null, icon:<EmptyWallet />},
         {label: 'News', path: '/news', targetSegment: null, icon:<DocumentText1 />},
         {label: 'Exchange', path: '/exchange', targetSegment: null, icon:<ArrangeHorizontalSquare />},
-    ];
+    ]},
+    {title: 'Account', links: [
+        {label: 'Notification', path: '/notification', targetSegment: null, icon:<Notification/>},
+        {label: 'Community', path: '/community', targetSegment: null, icon:<Profile2User />},
+        {label: 'Setting', path: '/settings', targetSegment: null, icon:<Setting2 />},
+    ]},
+    ]
+
+    const sidebarbottom =  [
+        {label: 'Support', path: '/support', targetSegment: null, icon:<Headphone/>},
+        {label: 'Messages', path: '/community', targetSegment: null, icon:<SmsEdit />},
+        {label: 'Logout', path: '/settings', targetSegment: null, icon:<Logout color="rgb(239 68 68)" />, red: true},
+    ]
 
     return(
         <aside className="fixed text-gray-500 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0">
@@ -19,49 +35,47 @@ export default function Sidebar(){
             <div className="logo text-2xl mb-4">
                 GoldDust.fi
             </div>
-            <p className="font-medium p-2 text-gray-500 my-2">Overview</p>
-            <ul className="space-y-4 font-medium ">
-                {links.map((l,i) => {
-                    return(
-                        <Link key={i} href={l.path} className="flex w-full items-center p-2 rounded-[6px] hover:bg-gray-100 group active:text-gray-200 active:bg-blue-500">
-                            {l.icon}
-                            <p className="ml-3">{l.label}</p>
-                        </Link>
-                    )
-                })}
-            </ul>
-            <p className="font-medium p-2 text-gray-500 my-2">Account</p>
-            <ul className="space-y-2 font-medium">
-                <li className="flex items-center p-2 rounded-[6px] hover:bg-gray-100 group">
-                    <Notification />
-                    <p className="ml-3">Notification</p>
-                </li>
-                <li className="flex items-center p-2 rounded-[6px] hover:bg-gray-100 group">
-                    <Profile2User />
-                    <p className="ml-3">Community</p>
-                </li>                
-                <li className="flex items-center p-2 rounded-[6px] hover:bg-gray-100 group">
-                    <Setting2 />
-                    <p className="ml-3">Setting</p>
-                </li>                
-            </ul>
+            <div>
+            {sidebar.map((l,i) => {
+                return(
+                <div key={i} className="">
+                <p className="font-medium p-2 text-gray-500 my-2">{l.title}</p>
+                <ul className="space-y-4 font-medium">
+                    {l.links.map((l,i) => {
+                        return(
+                            <Link key={i} href={l.path} className={pathname == l.path ? "flex w-full items-center p-2 rounded-[6px] group text-gray-200 bg-blue-500" : "flex w-full items-center p-2 rounded-[6px] hover:bg-gray-100 group"}>
+                                {l.icon}
+                                <p className="ml-3">{l.label}</p>
+                            </Link>
+                        )
+                    })}
+                </ul>
+                </div>)
+            })}
             </div>
+            </div>
+            <ul className="space-y-4 font-medium justify-flex-end w-full">
+                    {sidebarbottom.map((l,i) => {
+                        if (l.red){
+                            return(
+                                <Link key={i} href={l.path} className={pathname == l.path ? "flex w-full items-center p-2 rounded-[6px] group" : "flex w-full items-center p-2 rounded-[6px] text-red-500 bg-red-100 group"}>
+                                    {l.icon}
+                                    <p className="ml-3">{l.label}</p>
+                                </Link>
+                            ) 
+                        }
 
-            <ul className="space-y-2 font-medium justify-flex-end">
-                <li className="flex items-center p-3 rounded-[6px] hover:bg-gray-100 group">
-                    <Headphone />
-                    <p className="ml-3">Support</p>
-                </li>
-                <li className="flex items-center p-3 rounded-[6px] hover:bg-gray-100 group">
-                    <SmsEdit />
-                    <p className="ml-3">Messages</p>
-                </li>                
-                <li className="flex items-center p-3 text-red-500 bg-red-100 rounded-[6px] hover:bg-gray-100 group">
-                    <LogoutCurve color="rgb(239 68 68)"/>
-                    <p className="ml-3">Logout</p>
-                </li>                
+                        return(
+                            <Link key={i} href={l.path} className={pathname == l.path ? "flex w-full items-center p-2 rounded-[6px] group text-gray-200 bg-blue-500" : "flex w-full items-center p-2 rounded-[6px] hover:bg-gray-100 group"}>
+                                {l.icon}
+                                <p className="ml-3">{l.label}</p>
+                            </Link>
+                        )
+                    })}
             </ul>
+
             </div>
         </aside>
     )
 }
+
